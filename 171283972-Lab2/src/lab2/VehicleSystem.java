dd8f5365bd67d0756d1145e167550e4dd4c3258f
faiyazkhanwif <faiyazkhanwif@gmail.com>
2020-10-23 12:04:00 +0800

    Vehicle System Updated

--- 171283972-Lab2/src/lab2/VehicleSystem.java
+++ 171283972-Lab2/src/lab2/VehicleSystem.java
@@ -13,6 +13,43 @@ import java.util.Scanner;
  */
 public class VehicleSystem {
 
+    public static void systemworking(int n, boolean first, Vehicle vh, Scanner in) {
+        while (n != 4) {
+            if (first == true) {
+                vh.display();
+                vh.performmove();
+            }
+            System.out.println("    Type 1 if you want me to drive, 2 to fly, 3 to exit choosing my moving behavior:");
+            int m = in.nextInt();
+            if (m == 1) {
+                if (n == 1 || n == 2) {
+                    vh.display();
+                    vh.performmove();
+                } else {
+                    vh.setMoveBehavior(new MoveOnGround());
+                    vh.display();
+                    vh.performmove();
+                }
+                first = false;
+            } else if (m == 2) {
+                if (n == 1 || n == 2) {
+                    vh.display();
+                    System.out.println("I cannot fly.");
+                } else {
+                    vh.setMoveBehavior(new MoveInAir());
+                    vh.display();
+                    vh.performmove();
+                }
+                first = false;
+            } else if (m == 3) {
+                break;
+            } else {
+                System.out.println("Invalid choice of moving behavior. Try again");
+                first = false;
+            }
+        }
+    }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         Vehicle sr = new StreetRacer();
@@ -23,78 +60,15 @@ public class VehicleSystem {
             System.out.println("Type 1 for StreetRacer, 2 for FormulaOne, 3 for AirCraft, 4 to exit: ");
             int n = in.nextInt();
             boolean first = true;
-            while (n == 1) {
-                if (first == true) {
-                    sr.display();
-                    sr.performmove();
-                }
-                System.out.println("    Type 1 if you want me to drive, 2 to fly, 3 to exit choosing my moving behavior:");
-                int m = in.nextInt();
-                if (m == 1) {
-                    sr.display();
-                    sr.performmove();
-                    first = false;
-                } else if (m == 2) {
-                    sr.display();
-                    System.out.println("I cannot fly.");
-                    first = false;
-                } else if (m == 3) {
-                    break;
-                } else {
-                    System.out.println("Invalid choice of moving behavior. Try again");
-                    first = false;
-                }
-            }
-            while (n == 2) {
-                if (first == true) {
-                    fo.display();
-                    fo.performmove();
-                }
-                System.out.println("    Type 1 if you want me to drive, 2 to fly, 3 to exit choosing my moving behavior:");
-                int m = in.nextInt();
-                if (m == 1) {
-                    fo.display();
-                    fo.performmove();
-                    first = false;
-                } else if (m == 2) {
-                    fo.display();
-                    System.out.println("I cannot fly.");
-                    first = false;
-                } else if (m == 3) {
-                    break;
-                } else {
-                    System.out.println("Invalid choice of moving behavior. Try again");
-                    first = false;
-                }
-            }
-            while (n == 3) {
-                if (first == true) {
-                    ar.display();
-                    ar.performmove();
-                }
-                System.out.println("    Type 1 if you want me to drive, 2 to fly, 3 to exit choosing my moving behavior:");
-                int m = in.nextInt();
-                if (m == 1) {
-                    ar.setMoveBehavior(new MoveOnGround());
-                    ar.display();
-                    ar.performmove();
-                    first = false;
-                } else if (m == 2) {
-                    ar.setMoveBehavior(new MoveInAir());
-                    ar.display();
-                    ar.performmove();
-                    first = false;
-                } else if (m == 3) {
-                    break;
-                } else {
-                    System.out.println("Invalid choice of moving behavior. Try again");
-                    first = false;
-                }
-            }
-            if (n == 4) {
+            if (n == 1) {
+                systemworking(n, first, sr, in);
+            } else if (n == 2) {
+                systemworking(n, first, fo, in);
+            } else if (n == 3) {
+                systemworking(n, first, ar, in);
+            } else if (n == 4) {
                 x = 1;
-            }else if (n > 4 || n<0) {
-                System.out.println(n);
+            } else {
                 System.out.println("Invalid choice of type of vehicle. Try again");
             }
         }
